01808cb3327669314847efe1ec8b2d7585a3044c
SurajDadral <dadralj18@gmail.com>
2019-05-13 15:39:51 +0530

    Implement front end of UI
    - Create ColumnReinforcement.py file, which implement front end of UI.
    - Add temporary icons to show in UI.
    
    How to use?
    
    - Open new file and select face of column.
    - In FreeCAD's python console, paste following code snippet:
    
    from ColumnReinforcement import ColumnReinforcement
    FreeCADGui.Control.showDialog(ColumnReinforcement._ColumnTaskPanel())

---
+++ ColumnReinforcement/ColumnReinforcement.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2019 - Suraj <dadralj18@gmail.com>                      *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+__title__ = "Column Reinforcement"
+__author__ = "Suraj"
+__url__ = "https://www.freecadweb.org"
+
+from RebarDistribution import runRebarDistribution, removeRebarDistribution
+from Rebarfunc import getSelectedFace
+from PySide import QtGui
+import FreeCADGui
+import os
+
+
+class _ColumnTaskPanel:
+    def __init__(self, Rebar=None):
+        self.customSpacing = None
+        if not Rebar:
+            selected_obj = FreeCADGui.Selection.getSelectionEx()[0]
+            self.SelectedObj = selected_obj.Object
+            self.FaceName = selected_obj.SubElementNames[0]
+        else:
+            self.FaceName = Rebar.Base.Support[0][1][0]
+            self.SelectedObj = Rebar.Base.Support[0][0]
+        self.form = FreeCADGui.PySideUic.loadUi(os.path.splitext(__file__)[0] + ".ui")
+        self.form.setWindowTitle(
+            QtGui.QApplication.translate("RebarAddon", "Column Reinforcement", None)
+        )
+        self.form.image.setPixmap(
+            QtGui.QPixmap(
+                os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+                + "/icons/Column_CustomConfiguration.png"
+            )
+        )
+        self.addDropdownMenuItems()
+        self.connectSignalSlots()
+
+    def addDropdownMenuItems(self):
+        self.form.columnConfiguration.addItems(
+            ["Custom Configuration", "SingleTieFourRebars"]
+        )
+        self.form.bentAngle.addItems(["90", "135"])
+        self.form.mainRebarType.addItems(["StraightRebar", "LShapeRebar"])
+        self.form.x_dirRebarType.addItems(["StraightRebar", "LShapeRebar"])
+        self.form.y_dirRebarType.addItems(["StraightRebar", "LShapeRebar"])
+
+    def connectSignalSlots(self):
+        self.form.columnConfiguration.currentIndexChanged.connect(
+            self.changeColumnConfiguration
+        )
+        self.form.amount_radio.clicked.connect(self.amount_radio_clicked)
+        self.form.spacing_radio.clicked.connect(self.spacing_radio_clicked)
+        self.form.customSpacing.clicked.connect(lambda: runRebarDistribution(self))
+        self.form.removeCustomSpacing.clicked.connect(
+            lambda: removeRebarDistribution(self)
+        )
+        self.form.PickSelectedFace.clicked.connect(lambda: getSelectedFace(self))
+
+    def getStandardButtons(self):
+        return (
+            int(QtGui.QDialogButtonBox.Ok)
+            | int(QtGui.QDialogButtonBox.Apply)
+            | int(QtGui.QDialogButtonBox.Cancel)
+            | int(QtGui.QDialogButtonBox.Help)
+            | int(QtGui.QDialogButtonBox.Reset)
+        )
+
+    def changeColumnConfiguration(self):
+        column_configuration = self.form.columnConfiguration.currentText()
+        if column_configuration == "Custom Configuration":
+            self.form.image.setPixmap(
+                QtGui.QPixmap(
+                    os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+                    + "/icons/Column_CustomConfiguration.png"
+                )
+            )
+            # if self.form.columnConfiguration.oldText() == "SingleTieFourRebars":
+            self.showXdirRebarsForm()
+            self.showYdirRebarsForm()
+        elif column_configuration == "SingleTieFourRebars":
+            self.form.image.setPixmap(
+                QtGui.QPixmap(
+                    os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+                    + "/icons/Column_SingleTieFourRebars.png"
+                )
+            )
+            self.hideXdirRebarsForm()
+            self.hideYdirRebarsForm()
+
+    def amount_radio_clicked(self):
+        self.form.spacing.setEnabled(False)
+        self.form.amount.setEnabled(True)
+
+    def spacing_radio_clicked(self):
+        self.form.amount.setEnabled(False)
+        self.form.spacing.setEnabled(True)
+
+    def hideXdirRebarsForm(self):
+        self.form.x_dirRebarsWidget.hide()
+
+    def hideYdirRebarsForm(self):
+        self.form.y_dirRebarsWidget.hide()
+
+    def showXdirRebarsForm(self):
+        self.form.x_dirRebarsWidget.show()
+
+    def showYdirRebarsForm(self):
+        self.form.y_dirRebarsWidget.show()
