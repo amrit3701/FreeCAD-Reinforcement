763a5a9f0afa03923d4c9343ddeafcd6fc52cdcf
SurajDadral <dadralj18@gmail.com>
2019-07-05 09:40:43 +0530

    Create function to edit xdir/ydir rebars
    In previous implementation:
    - Every time xdir/ydir rebars are deleted and recreated, when user open
    dialog box to edit column reinforcement. No matter if user change any
    parameter or not, xdir/ydir rebars were always recreated.
    
    Now:
    - xdir/ydir rebars will only be recreated when user changes the "rebars
    type" or "number#diameter" of xdir/ydir rebars, otherwise they will be
    edited.

--- ColumnReinforcement/SingleTieMultipleRebars.py
+++ ColumnReinforcement/SingleTieMultipleRebars.py
@@ -34,7 +34,7 @@ from ColumnReinforcement.SingleTie import (
     getFacenameforRebar,
     getLRebarOrientationLeftRightCover,
 )
-from StraightRebar import makeStraightRebar
+from StraightRebar import makeStraightRebar, editStraightRebar
 from LShapeRebar import makeLShapeRebar, editLShapeRebar
 from Rebarfunc import getParametersOfFace, gettupleOfNumberDiameter
 
@@ -425,6 +425,7 @@ def makeXDirRebars(
                 f_cover_of_xdir_rebars += (
                     number * dia + number * spacing_in_xdir_rebars
                 )
+    FreeCAD.ActiveDocument.recompute()
     return xdir_rebars
 
 
@@ -589,6 +590,7 @@ def makeYDirRebars(
                 f_cover_of_ydir_rebars += (
                     number * dia + number * spacing_in_ydir_rebars
                 )
+    FreeCAD.ActiveDocument.recompute()
     return ydir_rebars
 
 
@@ -765,15 +767,56 @@ def editSingleTieMultipleRebars(
     xdir_rebars = xdir_rebars_group.XDirRebars
     ydir_rebars = ydir_rebars_group.YDirRebars
 
-    # Delete previously created xdir rebars
-    for Rebar in xdir_rebars:
-        base_name = Rebar.Base.Name
-        FreeCAD.ActiveDocument.removeObject(Rebar.Name)
-        FreeCAD.ActiveDocument.removeObject(base_name)
-    FreeCAD.ActiveDocument.recompute()
+    # Check if there is need to recreate rebars
+    recreate_Xdir_rebars = False
+    recreate_ydir_rebars = False
+    prev_xdir_rebars_type = xdir_rebars_group.RebarType
+    prev_xdir_rebars_number_diameter = xdir_rebars_group.NumberDiameter
+    prev_ydir_rebars_type = ydir_rebars_group.RebarType
+    prev_ydir_rebars_number_diameter = ydir_rebars_group.NumberDiameter
+    if (
+        prev_xdir_rebars_type == xdir_rebars_type
+        and prev_xdir_rebars_number_diameter == xdir_rebars_number_diameter
+    ):
+        recreate_xdir_rebars = False
+    else:
+        recreate_xdir_rebars = True
+    if (
+        prev_ydir_rebars_type == ydir_rebars_type
+        and prev_ydir_rebars_number_diameter == ydir_rebars_number_diameter
+    ):
+        recreate_ydir_rebars = False
+    else:
+        recreate_ydir_rebars = True
 
-    if xdir_rebars_number_diameter and xdir_rebars_number_diameter != "0":
-        xdir_rebars = makeXDirRebars(
+    if recreate_xdir_rebars:
+        for Rebar in xdir_rebars:
+            base_name = Rebar.Base.Name
+            FreeCAD.ActiveDocument.removeObject(Rebar.Name)
+            FreeCAD.ActiveDocument.removeObject(base_name)
+        FreeCAD.ActiveDocument.recompute()
+
+        if xdir_rebars_number_diameter and xdir_rebars_number_diameter != "0":
+            xdir_rebars = makeXDirRebars(
+                l_cover_of_tie,
+                r_cover_of_tie,
+                t_cover_of_tie,
+                b_cover_of_tie,
+                dia_of_tie,
+                dia_of_main_rebars,
+                xdir_rebars_t_offset,
+                xdir_rebars_b_offset,
+                xdir_rebars_type,
+                xdir_hook_orientation,
+                xdir_hook_extension,
+                l_xdir_rebar_rounding,
+                xdir_rebars_number_diameter,
+                facename,
+                structure,
+            )
+    else:
+        editXDirRebars(
+            xdir_rebars,
             l_cover_of_tie,
             r_cover_of_tie,
             t_cover_of_tie,
@@ -791,15 +834,34 @@ def editSingleTieMultipleRebars(
             structure,
         )
 
-    # Delete previously created ydir rebars
-    for Rebar in ydir_rebars:
-        base_name = Rebar.Base.Name
-        FreeCAD.ActiveDocument.removeObject(Rebar.Name)
-        FreeCAD.ActiveDocument.removeObject(base_name)
-    FreeCAD.ActiveDocument.recompute()
+    if recreate_ydir_rebars:
+        for Rebar in ydir_rebars:
+            base_name = Rebar.Base.Name
+            FreeCAD.ActiveDocument.removeObject(Rebar.Name)
+            FreeCAD.ActiveDocument.removeObject(base_name)
+        FreeCAD.ActiveDocument.recompute()
 
-    if ydir_rebars_number_diameter and ydir_rebars_number_diameter != "0":
-        ydir_rebars = makeYDirRebars(
+        if ydir_rebars_number_diameter and ydir_rebars_number_diameter != "0":
+            ydir_rebars = makeYDirRebars(
+                l_cover_of_tie,
+                r_cover_of_tie,
+                t_cover_of_tie,
+                b_cover_of_tie,
+                dia_of_tie,
+                dia_of_main_rebars,
+                ydir_rebars_t_offset,
+                ydir_rebars_b_offset,
+                ydir_rebars_type,
+                ydir_hook_orientation,
+                ydir_hook_extension,
+                l_ydir_rebar_rounding,
+                ydir_rebars_number_diameter,
+                facename,
+                structure,
+            )
+    else:
+        editYDirRebars(
+            ydir_rebars,
             l_cover_of_tie,
             r_cover_of_tie,
             t_cover_of_tie,
@@ -816,7 +878,6 @@ def editSingleTieMultipleRebars(
             facename,
             structure,
         )
-    FreeCAD.ActiveDocument.recompute()
 
     # Add created xdir/ydir rebars to xdir_rebars_group/ydir_rebars_group
     xdir_rebars_group.addObjects(xdir_rebars)
@@ -844,6 +905,344 @@ def editSingleTieMultipleRebars(
 
     FreeCAD.ActiveDocument.recompute()
     return rebar_group
+
+
+def editXDirRebars(
+    xdir_rebars_list,
+    l_cover_of_tie,
+    r_cover_of_tie,
+    t_cover_of_tie,
+    b_cover_of_tie,
+    dia_of_tie,
+    dia_of_main_rebars,
+    xdir_rebars_t_offset,
+    xdir_rebars_b_offset,
+    xdir_rebars_type,
+    xdir_hook_orientation,
+    xdir_hook_extension,
+    l_xdir_rebar_rounding,
+    xdir_rebars_number_diameter,
+    facename,
+    structure,
+):
+    # Find parameters of selected face
+    FacePRM = getParametersOfFace(structure, facename)
+
+    facename_for_xdir_rebars = getFacenameforRebar(
+        "y-axis", facename, structure
+    )
+
+    # find list of tuples of number and diameter of xdir rebars
+    xdir_rebars_number_diameter_list = gettupleOfNumberDiameter(
+        xdir_rebars_number_diameter
+    )
+    xdir_rebars_number_diameter_list.reverse()
+
+    # Calculate spacing between xdir-rebars
+    face_length = FacePRM[0][0]
+    xdir_span_length = (
+        FacePRM[0][0]
+        - l_cover_of_tie
+        - r_cover_of_tie
+        - 2 * dia_of_tie
+        - 2 * dia_of_main_rebars
+    )
+    req_space_for_xdir_rebars = sum(
+        x[0] * x[1] for x in xdir_rebars_number_diameter_list
+    )
+    xdir_rebars_number = sum(
+        number for number, _ in xdir_rebars_number_diameter_list
+    )
+    spacing_in_xdir_rebars = (xdir_span_length - req_space_for_xdir_rebars) / (
+        xdir_rebars_number + 1
+    )
+
+    # Set parameter values for Straight/LShape xdir_rebars
+    list_coverAlong = ["Right Side", "Left Side"]
+
+    if xdir_rebars_type == "StraightRebar":
+        r_cover = t_cover_of_tie + dia_of_tie
+        l_cover = b_cover_of_tie + dia_of_tie
+        rl_cover = [r_cover, l_cover]
+
+        index = 0
+        for i, coverAlong in enumerate(list_coverAlong):
+            for j, (number, dia) in enumerate(xdir_rebars_number_diameter_list):
+                if j == 0:
+                    f_cover_of_xdir_rebars = (
+                        r_cover_of_tie
+                        + dia_of_tie
+                        + dia_of_main_rebars
+                        + spacing_in_xdir_rebars
+                    )
+                rear_cover_of_xdir_rebars = (
+                    FacePRM[0][0]
+                    - f_cover_of_xdir_rebars
+                    - number * dia
+                    - (number - 1) * spacing_in_xdir_rebars
+                )
+
+                editStraightRebar(
+                    xdir_rebars_list[index],
+                    f_cover_of_xdir_rebars,
+                    (coverAlong, rl_cover[i]),
+                    xdir_rebars_t_offset,
+                    xdir_rebars_b_offset,
+                    dia,
+                    True,
+                    number,
+                    "Vertical",
+                    structure,
+                    facename_for_xdir_rebars,
+                )
+
+                xdir_rebars_list[index].OffsetEnd = (
+                    rear_cover_of_xdir_rebars + dia / 2
+                )
+                f_cover_of_xdir_rebars += (
+                    number * dia + number * spacing_in_xdir_rebars
+                )
+                index += 1
+    elif xdir_rebars_type == "LShapeRebar":
+        face_length = getParametersOfFace(structure, facename_for_xdir_rebars)[
+            0
+        ][0]
+        l_rebar_orientation_cover_list = []
+        for i, (number, dia_of_rebars) in enumerate(
+            xdir_rebars_number_diameter_list
+        ):
+            l_rebar_orientation_cover_list.append(
+                getLRebarOrientationLeftRightCover(
+                    xdir_hook_orientation,
+                    xdir_hook_extension,
+                    "y-axis",
+                    l_cover_of_tie,
+                    r_cover_of_tie,
+                    t_cover_of_tie,
+                    b_cover_of_tie,
+                    dia_of_tie,
+                    dia_of_rebars,
+                    l_xdir_rebar_rounding,
+                    face_length,
+                )
+            )
+        list_orientation = l_rebar_orientation_cover_list[0]["list_orientation"]
+        l_cover_list = []
+        for l_rebar_orientation_cover in l_rebar_orientation_cover_list:
+            l_cover_list.append(l_rebar_orientation_cover["l_cover"])
+
+        r_cover_list = []
+        for l_rebar_orientation_cover in l_rebar_orientation_cover_list:
+            r_cover_list.append(l_rebar_orientation_cover["r_cover"])
+
+        index = 0
+        for i, orientation in enumerate(list_orientation):
+            for j, (number, dia) in enumerate(xdir_rebars_number_diameter_list):
+                if j == 0:
+                    f_cover_of_xdir_rebars = (
+                        r_cover_of_tie
+                        + dia_of_tie
+                        + dia_of_main_rebars
+                        + spacing_in_xdir_rebars
+                    )
+                rear_cover_of_xdir_rebars = (
+                    FacePRM[0][0]
+                    - f_cover_of_xdir_rebars
+                    - number * dia
+                    - (number - 1) * spacing_in_xdir_rebars
+                )
+                editLShapeRebar(
+                    xdir_rebars_list[index],
+                    f_cover_of_xdir_rebars,
+                    xdir_rebars_b_offset,
+                    l_cover_list[j][i],
+                    r_cover_list[j][i],
+                    dia,
+                    xdir_rebars_t_offset,
+                    l_xdir_rebar_rounding,
+                    True,
+                    number,
+                    orientation,
+                    structure,
+                    facename_for_xdir_rebars,
+                )
+                xdir_rebars_list[index].OffsetEnd = (
+                    rear_cover_of_xdir_rebars + dia / 2
+                )
+                f_cover_of_xdir_rebars += (
+                    number * dia + number * spacing_in_xdir_rebars
+                )
+                index += 1
+    FreeCAD.ActiveDocument.recompute()
+
+
+def editYDirRebars(
+    ydir_rebars_list,
+    l_cover_of_tie,
+    r_cover_of_tie,
+    t_cover_of_tie,
+    b_cover_of_tie,
+    dia_of_tie,
+    dia_of_main_rebars,
+    ydir_rebars_t_offset,
+    ydir_rebars_b_offset,
+    ydir_rebars_type,
+    ydir_hook_orientation,
+    ydir_hook_extension,
+    l_ydir_rebar_rounding,
+    ydir_rebars_number_diameter,
+    facename,
+    structure,
+):
+    """Edits the rebars created along y-direction in the structural column
+    object."""
+    # Find parameters of selected face
+    FacePRM = getParametersOfFace(structure, facename)
+
+    facename_for_ydir_rebars = getFacenameforRebar(
+        "x-axis", facename, structure
+    )
+
+    # find list of tuples of number and diameter of ydir rebars
+    ydir_rebars_number_diameter_list = gettupleOfNumberDiameter(
+        ydir_rebars_number_diameter
+    )
+
+    # Calculate spacing between ydir-rebars
+    ydir_span_length = (
+        FacePRM[0][1]
+        - t_cover_of_tie
+        - b_cover_of_tie
+        - 2 * dia_of_tie
+        - 2 * dia_of_main_rebars
+    )
+    req_space_for_ydir_rebars = sum(
+        x[0] * x[1] for x in ydir_rebars_number_diameter_list
+    )
+    ydir_rebars_number = sum(
+        number for number, _ in ydir_rebars_number_diameter_list
+    )
+    spacing_in_ydir_rebars = (ydir_span_length - req_space_for_ydir_rebars) / (
+        ydir_rebars_number + 1
+    )
+
+    # Set parameter values for Straight/LShape ydir_rebars
+    list_coverAlong = ["Right Side", "Left Side"]
+
+    if ydir_rebars_type == "StraightRebar":
+        # Set parameter values for Straight ydir_rebars
+        r_cover = r_cover_of_tie + dia_of_tie
+        l_cover = l_cover_of_tie + dia_of_tie
+        rl_cover = [r_cover, l_cover]
+
+        index = 0
+        for i, coverAlong in enumerate(list_coverAlong):
+            for j, (number, dia) in enumerate(ydir_rebars_number_diameter_list):
+                if j == 0:
+                    f_cover_of_ydir_rebars = (
+                        b_cover_of_tie
+                        + dia_of_tie
+                        + dia_of_main_rebars
+                        + spacing_in_ydir_rebars
+                    )
+                rear_cover_of_ydir_rebars = (
+                    FacePRM[0][1]
+                    - f_cover_of_ydir_rebars
+                    - number * dia
+                    - (number - 1) * spacing_in_ydir_rebars
+                )
+
+                editStraightRebar(
+                    ydir_rebars_list[index],
+                    f_cover_of_ydir_rebars,
+                    (coverAlong, rl_cover[i]),
+                    ydir_rebars_t_offset,
+                    ydir_rebars_b_offset,
+                    dia,
+                    True,
+                    number,
+                    "Vertical",
+                    structure,
+                    facename_for_ydir_rebars,
+                )
+                ydir_rebars_list[index].OffsetEnd = (
+                    rear_cover_of_ydir_rebars + dia / 2
+                )
+                f_cover_of_ydir_rebars += (
+                    number * dia + number * spacing_in_ydir_rebars
+                )
+                index += 1
+    elif ydir_rebars_type == "LShapeRebar":
+        face_length = getParametersOfFace(structure, facename_for_ydir_rebars)[
+            0
+        ][0]
+        l_rebar_orientation_cover_list = []
+        for i, (number, dia_of_rebars) in enumerate(
+            ydir_rebars_number_diameter_list
+        ):
+            l_rebar_orientation_cover_list.append(
+                getLRebarOrientationLeftRightCover(
+                    ydir_hook_orientation,
+                    ydir_hook_extension,
+                    "x-axis",
+                    l_cover_of_tie,
+                    r_cover_of_tie,
+                    t_cover_of_tie,
+                    b_cover_of_tie,
+                    dia_of_tie,
+                    dia_of_rebars,
+                    l_ydir_rebar_rounding,
+                    face_length,
+                )
+            )
+        list_orientation = l_rebar_orientation_cover_list[0]["list_orientation"]
+        l_cover_list = []
+        for l_rebar_orientation_cover in l_rebar_orientation_cover_list:
+            l_cover_list.append(l_rebar_orientation_cover["l_cover"])
+
+        r_cover_list = []
+        for l_rebar_orientation_cover in l_rebar_orientation_cover_list:
+            r_cover_list.append(l_rebar_orientation_cover["r_cover"])
+
+        index = 0
+        for i, orientation in enumerate(list_orientation):
+            for j, (number, dia) in enumerate(ydir_rebars_number_diameter_list):
+                if j == 0:
+                    f_cover_of_ydir_rebars = (
+                        r_cover_of_tie
+                        + dia_of_tie
+                        + dia_of_main_rebars
+                        + spacing_in_ydir_rebars
+                    )
+                rear_cover_of_ydir_rebars = (
+                    FacePRM[0][1]
+                    - f_cover_of_ydir_rebars
+                    - number * dia
+                    - (number - 1) * spacing_in_ydir_rebars
+                )
+                editLShapeRebar(
+                    ydir_rebars_list[index],
+                    f_cover_of_ydir_rebars,
+                    ydir_rebars_b_offset,
+                    l_cover_list[j][i],
+                    r_cover_list[j][i],
+                    dia,
+                    ydir_rebars_t_offset,
+                    l_ydir_rebar_rounding,
+                    True,
+                    number,
+                    orientation,
+                    structure,
+                    facename_for_ydir_rebars,
+                )
+                ydir_rebars[index].OffsetEnd = (
+                    rear_cover_of_ydir_rebars + dia / 2
+                )
+                f_cover_of_ydir_rebars += (
+                    number * dia + number * spacing_in_ydir_rebars
+                )
+                index += 1
+    FreeCAD.ActiveDocument.recompute()
 
 
 class _SingleTieMultipleRebars:
