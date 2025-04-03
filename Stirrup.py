1c93c6dfa3fe9714f844fc808d37ec2c222d7fcb
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-04-03 18:18:34 +0100

    Update Stirrup.py fix button deprecated errors

--- Stirrup.py
+++ Stirrup.py
@@ -235,13 +235,13 @@ class _StirrupTaskPanel:
     @staticmethod
     def getStandardButtons():
         return (
-            int(QtGui.QDialogButtonBox.Ok)
-            | int(QtGui.QDialogButtonBox.Apply)
-            | int(QtGui.QDialogButtonBox.Cancel)
+            QtGui.QDialogButtonBox.Ok
+            | QtGui.QDialogButtonBox.Apply
+            | QtGui.QDialogButtonBox.Cancel
         )
 
     def clicked(self, button):
-        if button == int(QtGui.QDialogButtonBox.Apply):
+        if button == QtGui.QDialogButtonBox.Apply:
             self.accept(button)
 
     def accept(self, signal=None):
@@ -340,7 +340,7 @@ class _StirrupTaskPanel:
             rebar.CustomSpacing = self.CustomSpacing
             FreeCAD.ActiveDocument.recompute()
         self.Rebar = rebar
-        if signal == int(QtGui.QDialogButtonBox.Apply):
+        if signal == QtGui.QDialogButtonBox.Apply:
             pass
         else:
             FreeCADGui.Control.closeDialog()
