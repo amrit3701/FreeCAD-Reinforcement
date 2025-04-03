f348d8f22837c4e661d33e2b786dca95f84f33df
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-04-03 18:21:42 +0100

    Update BentShapeRebar.py fix button deprecated errors

--- BentShapeRebar.py
+++ BentShapeRebar.py
@@ -255,13 +255,13 @@ class _BentShapeRebarTaskPanel:
     @staticmethod
     def getStandardButtons():
         return (
-            int(QtGui.QDialogButtonBox.Ok)
-            | int(QtGui.QDialogButtonBox.Apply)
-            | int(QtGui.QDialogButtonBox.Cancel)
+            QtGui.QDialogButtonBox.Ok
+            | QtGui.QDialogButtonBox.Apply
+            | QtGui.QDialogButtonBox.Cancel
         )
 
     def clicked(self, button):
-        if button == int(QtGui.QDialogButtonBox.Apply):
+        if button == QtGui.QDialogButtonBox.Apply:
             self.accept(button)
 
     def accept(self, signal=None):
@@ -366,7 +366,7 @@ class _BentShapeRebarTaskPanel:
             rebar.CustomSpacing = self.CustomSpacing
             FreeCAD.ActiveDocument.recompute()
         self.Rebar = rebar
-        if signal == int(QtGui.QDialogButtonBox.Apply):
+        if signal == QtGui.QDialogButtonBox.Apply:
             pass
         else:
             FreeCADGui.Control.closeDialog()
